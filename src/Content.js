af6690547a59f0f2c2a6f6b98f594ee0c9cbe080
Hicham Thabti <thabti.hicham@gmail.com>
2023-02-20 11:35:52 +0100

    links added

--- src/Content.js
+++ src/Content.js
@@ -15,7 +15,8 @@ const Content = () => {
         Back-end : Node.js Express Mongoose / Front : ReactJS (some MUI)<br />
         The idea was to make a website that brought people together with all the differents events that the great city of Paris could offer. 
         <br />
-        (The back-end is also deployed on Vercel so it might be slow)
+        (The back-end is also deployed on Vercel so it might be slow)<br/>
+        <a className="repolinks" href="https://github.com/hich-t/ParisTogether_Front" target="_blank" rel="noreferrer noopener" >Repo Link</a>
         </p>
         </div>
         </div>
@@ -29,7 +30,8 @@ const Content = () => {
         <div className="projectdescription">
         <h2>Poke Arena (React)</h2>
         <p>My favourite project, especially design-wise, where I tried to re-create the atmosphere of the original game-boy games<br/> The concept is quite simple, you choose 4 pokemons amongst the 1st gen, then you can battle the four first gym leaders of the game.<br/>
-        The fight mechanic is still a WIP. Project made in 3 days.
+        The fight mechanic is still a WIP. Project made in 3 days.<br/>
+        <a className="repolinks" href="https://github.com/hich-t/Pokearena" target="_blank" rel="noreferrer noopener">Repo Link</a>
         </p>
         </div>
         </div>
@@ -44,7 +46,8 @@ const Content = () => {
         <p>One of the first projects I made at Ironhack<br />
         We made a simple little game, with JS, where you have to avoid
         obstacles by jumping above them. <br/>
-        Original character design.
+        Original character design.<br/>
+        <a className="repolinks" href="https://github.com/hich-t/project-week3-game" target="_blank" rel="noreferrer noopener">Repo Link</a>
         </p>
         </div>
         </div>
@@ -59,7 +62,9 @@ const Content = () => {
         <div className="projectdescription">
         <h2>Hackernews Clone (React)</h2>
         <p>Our first project with React<br />
-        We made a clone of the famous tech-news website Hackernews, using their API. 
+        We made a clone of the famous tech-news website Hackernews, using their API. <br/>
+        <a className="repolinks" href="https://github.com/hich-t/clonehackernews" target="_blank" rel="noreferrer noopener">Repo Link</a>
+
         </p>
         </div>
         </div>
@@ -71,7 +76,8 @@ const Content = () => {
         </a>
         <div className="projectdescription">
         <h2>Anim DB (React)</h2>
-        <p>A little project we made in 2 days, fetching data from an Anime API. 
+        <p>A little project we made in 2 days, fetching data from an Anime API. <br/>
+        <a className="repolinks" href="https://github.com/hich-t/Anim-DB" target="_blank" rel="noreferrer noopener">Repo Link</a>
         </p>
         </div>
         </div>
